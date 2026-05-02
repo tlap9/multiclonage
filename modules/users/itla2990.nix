78e2e31ce0c3d08c279aec5650f1a992994efd64
Tom Laplace-piedplat <tom.laplace-piedplat@francetravail.fr>
2026-05-02 10:26:15 +0200

    ajoute home directory pour ubuntu

--- modules/users/itla2990.nix
+++ modules/users/itla2990.nix
@@ -5,6 +5,7 @@
 {
   flake.modules.homeManager.itla2990 = {
     home.username = config.flake.username;
+    home.homeDirectory = "/home/${config.flake.username}";
     home.stateVersion = "26.05";
   };
 }
